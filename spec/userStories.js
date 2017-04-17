38e028a3e1e6692663c2d9e504bf9dea4395c869
Emily Chai <echai93@gmail.com>
2017-04-17 13:48:11 +0100

    20 user stories

---
+++ spec/userStories.js
@@ -0,0 +1,105 @@
+Bowling game user stories:
+
+Stakeholder
+Motivation
+Task
+
+1.
+As a player
+So that I can play a game bowling
+I want a game of bowling to have 10 frames
+
+2.
+As a player
+So that I can complete one frame of bowling
+I want to be able to roll two times
+
+3.
+As a player
+So that I can roll the ball in a frame
+I want to make sure that I have not already rolled twice
+
+4.
+As a player
+So that I can continue playing the bowling game
+I want to be able to move onto the next frame
+
+5.
+As a player
+So that I can move onto the next frame
+I want the frame to end after two rolls
+
+6.
+As a player
+So that I know when the frame ends
+I want to know the roll number
+
+7.
+As a player
+So that I know when the game is completed
+I want to know the frame number
+
+8.
+As a player
+So that I can knock down pins
+I want each frame to start with 10 pins
+
+9.
+As a player
+So that I can roll the next frame
+I want the pins to be reset after every frame
+
+10.
+As a player
+So that I can roll a strike
+I want to be able to knock down 10 pins with the first roll
+
+11.
+As a player
+So that I can roll a spare
+I want to be able to knock down 10 pins with the second roll
+
+12.
+As a player
+So that I can move onto the next frame after a strike
+I want the frame to end
+
+13.
+As a player
+So that I can win a strike bonus
+I want to calculate the number of pins knocked down in the next two rolls (next frame)
+
+14.
+As a player
+So that I can win a spare bonus
+I want to calculate the number of pins knocked down by the next roll (1st roll of next frame)
+
+15.
+As a player
+So that I can roll additional balls for a bonus
+I want to roll a strike or spare in the final frame
+
+16.
+As a player
+So that I can win a bonus in the final frame
+I want to make sure that I do not roll more than 3 times
+
+17.
+As a player
+So that I can win a gutter game
+I want to score 0 points for the entire game
+
+18.
+As a player
+So that I can win a perfect game
+I want to roll 12 strikes
+
+19.
+As a player
+So that the score can be calculated
+I want to know how many pins have been knocked down after each roll
+
+20.
+As a player
+So that I know the score at the end of the game
+I want to calculate the total score

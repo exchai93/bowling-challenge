5fb06d19c94ce469717b67359b594cf2a273acaf
Emily Chai <echai93@gmail.com>
2017-04-17 21:05:34 +0100

    WPI: tests

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -8,7 +8,26 @@ describe ('One frame', function(){
     frame = new Frame();
   });
 
+  it('has 10 pins by default', function(){
+    expect(frame.pins).toEqual(10);
+  });
 
+  describe('Rolls', function(){
+    it('has a first roll', function(){
+      expect(frame.firstRoll()).toBeTruthy();
+    });
+    it('has a second roll', function(){
+      expect(frame.secondRoll()).toBeTruthy();
+    });
+    it('condition to roll', function(){
+      expect(frame.conditionToRoll()).toBeTruthy();
+    });
+  });
 
-
+  describe('Next frame', function(){
+    it('sets a new frame', function(){
+      newFrame = new Frame();
+      expect()
+    });
+  });
 });
